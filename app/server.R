1a3937de94b568b0a2eab9a2e3efce04c631fab3
JiansongChen2018 <jc4858@columbia.com>
2018-10-07 16:36:35 -0400

    Update

--- app/server.R
+++ app/server.R
@@ -1,168 +1,227 @@
-library(shiny)
-library(choroplethr)
-library(choroplethrZip)
+
+packages.used=c("leaflet","geosphere","shiny","dplyr","shinyjs")
+
+# check packages that need to be installed.
+packages.needed=setdiff(packages.used,
+                        intersect(installed.packages()[,1],
+                                  packages.used))
+# install additional packages
+if(length(packages.needed)>0){
+  install.packages(packages.needed, dependencies = TRUE)
+}
+
+
 library(dplyr)
 library(leaflet)
-library(maps)
-library(rgdal)
+library(shiny)
+library(shinyjs)
 
-## Define Manhattan's neighborhood
-man.nbhd=c("all neighborhoods", "Central Harlem", 
-           "Chelsea and Clinton",
-           "East Harlem", 
-           "Gramercy Park and Murray Hill",
-           "Greenwich Village and Soho", 
-           "Lower Manhattan",
-           "Lower East Side", 
-           "Upper East Side", 
-           "Upper West Side",
-           "Inwood and Washington Heights")
-zip.nbhd=as.list(1:length(man.nbhd))
-zip.nbhd[[1]]=as.character(c(10026, 10027, 10030, 10037, 10039))
-zip.nbhd[[2]]=as.character(c(10001, 10011, 10018, 10019, 10020))
-zip.nbhd[[3]]=as.character(c(10036, 10029, 10035))
-zip.nbhd[[4]]=as.character(c(10010, 10016, 10017, 10022))
-zip.nbhd[[5]]=as.character(c(10012, 10013, 10014))
-zip.nbhd[[6]]=as.character(c(10004, 10005, 10006, 10007, 10038, 10280))
-zip.nbhd[[7]]=as.character(c(10002, 10003, 10009))
-zip.nbhd[[8]]=as.character(c(10021, 10028, 10044, 10065, 10075, 10128))
-zip.nbhd[[9]]=as.character(c(10023, 10024, 10025))
-zip.nbhd[[10]]=as.character(c(10031, 10032, 10033, 10034, 10040))
+museum<-na.omit(read.csv("C:/Users/cjsly/Documents/GitHub/Fall2017-project2-grp5/data/museums.csv"))
+theatre<-read.csv('C:/Users/cjsly/Documents/GitHub/Fall2017-project2-grp5/data/theatre.csv')
+gallery<-read.csv("C:/Users/cjsly/Documents/GitHub/Fall2017-project2-grp5/data/Gallery.csv")
+library<-read.csv('C:/Users/cjsly/Documents/GitHub/Fall2017-project2-grp5/data/library.csv')
 
-## Load housing data
-load("../output/count.RData")
-load("../output/mh2009use.RData")
+restaurant<- read.csv('C:/Users/cjsly/Documents/GitHub/Fall2017-project2-grp5/data/restaurant_new.csv')
+icon_museum<-icons(iconUrl = 'icon_museum.png',iconHeight = 15, iconWidth = 15)
+icon_theatre<-icons(iconUrl = 'icon_theatre.png', iconHeight = 18, iconWidth = 18)
+
+icon_rest<-icons(iconUrl =  'icon_rest.png', iconHeight=25, iconWidth = 25)
+
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
-  ## Neighborhood name
-  output$text = renderText({"Selected:"})
-  output$text1 = renderText({
-      paste("{ ", man.nbhd[as.numeric(input$nbhd)+1], " }")
-  })
+
+   output$map<-renderLeaflet({
+     leaflet() %>% setView(lng=-73.93, lat=40.77, zoom=10) %>% addTiles()
+   })
   
-  ## Panel 1: summary plots of time trends, 
-  ##          unit price and full price of sales. 
-  
-  output$distPlot <- renderPlot({
+   observeEvent(input$destination, {
+     if('Museum'==input$destination){
+       
+       leafletProxy('map') %>% clearMarkerClusters() %>% setView(lng=-73.93, lat=40.77, zoom=12) %>% 
+         addMarkers(museum$longtitude, 
+                    museum$latitude, icon=icon_museum,
+                    clusterOptions=markerClusterOptions(),
+                    popup=paste('Name:',museum$NAME, '<br/>',
+                                'Tel:', museum$TEL, '<br/>',
+                                'Zip:', museum$ZIP, '<br/>',
+                                'Website:', a(museum$URL, href=museum$URL), '<br/>', 
+                                'Add:', museum$ADRESS1, '<br/>'
+                                )
+                   
+                    )
+     }
+     else if('Theater'==input$destination)
+     {
+       leafletProxy('map') %>% clearMarkerClusters() %>% setView(lng=-73.93, lat=40.77, zoom=12) %>% 
+         addMarkers(theatre$longtitude,
+                    theatre$latitude, icon=icon_theatre,
+                    clusterOptions=markerClusterOptions(),
+                    popup=paste('Name:', theatre$NAME, '<br/>',
+                                'Tel:', theatre$TEL, '<br/>',
+                                'Zip:', theatre$ZIP, '<br/>',
+                                'Website:', a(theatre$URL, href=theatre$URL), '<br/>',
+                                'Add:', theatre$ADDRESS1, '<br/>'))
+     }
+     else if ('Gallery'==input$destination)
+     {
+       leafletProxy('map') %>% clearMarkerClusters() %>% setView(lng=-73.93, lat=40.77, zoom=12) %>% 
+         addMarkers(gallery$LON,
+                    gallery$LAT, icon=icon_theatre,
+                    clusterOptions=markerClusterOptions(),
+                    popup=paste('Name:', gallery$NAME, '<br/>',
+                                'Tel:', gallery$TEL, '<br/>',
+                                'Zip:', gallery$ZIP, '<br/>',
+                                'Website:', a(gallery$URL, href=gallery$URL), '<br/>',
+                                'Add:', gallery$ADDRESS1, '<br/>'))
+     }
+     else {
+       leafletProxy('map') %>% clearMarkerClusters() %>% setView(lng=-73.93, lat=40.77, zoom=12) %>% 
+         addMarkers(library$LON,
+                    library$LAT, icon=icon_theatre,
+                    clusterOptions=markerClusterOptions(),
+                    popup=paste('Name:', library$NAME, '<br/>',
+                                'Zip:', library$ZIP, '<br/>',
+                                'Website:', a(library$URL, href=library$URL), '<br/>',
+                                'Add:', library$ADDRESS1, '<br/>'))
+     }
     
-    ## First filter data for selected neighborhood
-    mh2009.sel=mh2009.use
-    if(input$nbhd>0){
-      mh2009.sel=mh2009.use%>%
-                  filter(region %in% zip.nbhd[[as.numeric(input$nbhd)]])
-    }
-    
-    ## Monthly counts
-    month.v=as.vector(table(mh2009.sel$sale.month))
-    
-    ## Price: unit (per sq. ft.) and full
-    type.price=data.frame(bldg.type=c("10", "13", "25", "28"))
-    type.price.sel=mh2009.sel%>%
-                group_by(bldg.type)%>%
-                summarise(
-                  price.mean=mean(sale.price, na.rm=T),
-                  price.median=median(sale.price, na.rm=T),
-                  unit.mean=mean(unit.price, na.rm=T),
-                  unit.median=median(unit.price, na.rm=T),
-                  sale.n=n()
-                )
-    type.price=left_join(type.price, type.price.sel, by="bldg.type")
-    
-    ## Making the plots
-    layout(matrix(c(1,1,1,1,2,2,3,3,2,2,3,3), 3, 4, byrow=T))
-    par(cex.axis=1.3, cex.lab=1.5, 
-        font.axis=2, font.lab=2, col.axis="dark gray", bty="n")
-    
-    ### Sales monthly counts
-    plot(1:12, month.v, xlab="Months", ylab="Total sales", 
-         type="b", pch=21, col="black", bg="red", 
-         cex=2, lwd=2, ylim=c(0, max(month.v,na.rm=T)*1.05))
-    
-    ### Price per square foot
-    plot(c(0, max(type.price[,c(4,5)], na.rm=T)), 
-         c(0,5), 
-         xlab="Price per square foot", ylab="", 
-         bty="l", type="n")
-    text(rep(0, 4), 1:4+0.5, paste(c("coops", "condos", "luxury hotels", "comm. condos"), 
-                                  type.price$sale.n, sep=": "), adj=0, cex=1.5)
-    points(type.price$unit.mean, 1:nrow(type.price), pch=16, col=2, cex=2)
-    points(type.price$unit.median, 1:nrow(type.price),  pch=16, col=4, cex=2)
-    segments(type.price$unit.mean, 1:nrow(type.price), 
-              type.price$unit.median, 1:nrow(type.price),
-             lwd=2)    
-    
-    ### full price
-    plot(c(0, max(type.price[,-1], na.rm=T)), 
-         c(0,5), 
-         xlab="Sales Price", ylab="", 
-         bty="l", type="n")
-    text(rep(0, 4), 1:4+0.5, paste(c("coops", "condos", "luxury hotels", "comm. condos"), 
-                                   type.price$sale.n, sep=": "), adj=0, cex=1.5)
-    points(type.price$price.mean, 1:nrow(type.price), pch=16, col=2, cex=2)
-    points(type.price$price.median, 1:nrow(type.price),  pch=16, col=4, cex=2)
-    segments(type.price$price.mean, 1:nrow(type.price), 
-             type.price$price.median, 1:nrow(type.price),
-             lwd=2)    
-  })
-  
-  ## Panel 2: map of sales distribution
-  output$distPlot1 <- renderPlot({
-    count.df.sel=count.df
-    if(input$nbhd>0){
-      count.df.sel=count.df%>%
-        filter(region %in% zip.nbhd[[as.numeric(input$nbhd)]])
-    }
-    # make the map for selected neighhoods
-    
-    zip_choropleth(count.df.sel,
-                   title       = "2009 Manhattan housing sales",
-                   legend      = "Number of sales",
-                   county_zoom = 36061)
-  })
-  
-  ## Panel 3: leaflet
-  output$map <- renderLeaflet({
-    count.df.sel=count.df
-    if(input$nbhd>0){
-      count.df.sel=count.df%>%
-        filter(region %in% zip.nbhd[[as.numeric(input$nbhd)]])
-    }
-    
-    # From https://data.cityofnewyork.us/Business/Zip-Code-Boundaries/i8iw-xf4u/data
-    NYCzipcodes <- readOGR("../data/ZIP_CODE_040114.shp",
-                           #layer = "ZIP_CODE", 
-                           verbose = FALSE)
-    
-    selZip <- subset(NYCzipcodes, NYCzipcodes$ZIPCODE %in% count.df.sel$region)
-    
-    # ----- Transform to EPSG 4326 - WGS84 (required)
-    subdat<-spTransform(selZip, CRS("+init=epsg:4326"))
-    
-    # ----- save the data slot
-    subdat_data=subdat@data[,c("ZIPCODE", "POPULATION")]
-    subdat.rownames=rownames(subdat_data)
-    subdat_data=
-      subdat_data%>%left_join(count.df, by=c("ZIPCODE" = "region"))
-    rownames(subdat_data)=subdat.rownames
-    
-    # ----- to write to geojson we need a SpatialPolygonsDataFrame
-    subdat<-SpatialPolygonsDataFrame(subdat, data=subdat_data)
-    
-    # ----- set uo color pallette https://rstudio.github.io/leaflet/colors.html
-    # Create a continuous palette function
-    pal <- colorNumeric(
-      palette = "Blues",
-      domain = subdat$POPULATION
-    )
-    
-    leaflet(subdat) %>%
-      addTiles()%>%
-      addPolygons(
-        stroke = T, weight=1,
-        fillOpacity = 0.6,
-        color = ~pal(POPULATION)
-      )
-  })
+   })
+   
+   observe({
+     click<- input$map_marker_click
+     if(is.null(click))
+       return() 
+     ## use click to access the zoom and set the view according this marker
+     leafletProxy('map') %>% setView(lng=click$lng, 
+                                     lat=click$lat,
+                                     zoom=15)
+   })
+   
+   observeEvent(input$button2,{
+     leafletProxy('map') %>% setView(lng=-73.93, lat=40.77, zoom=12)
+   })
+   
+     observeEvent(input$map_marker_click, {
+       show('Sure')
+     })
+   
+   
+     observeEvent(input$button1, {
+       show('Resta')
+     })
+     
+     observeEvent(input$button3,{
+       show('Recom')
+     })
+
+     
+     observeEvent(input$button3, {
+         click<- input$map_marker_click
+         type<- input$type
+         rank<- input$rank
+         if(is.null(type))
+           return()
+         lng<- click$lng
+         lat<- click$lat
+         if(rank=='by Distance'){
+           
+           add_to_df<- reactive({
+             rest <-  restaurant %>% dplyr::filter(TYPE==type) %>% 
+               mutate(dist = sqrt((LON-lng)^2 + (LAT-lat)^2)) %>% arrange(dist)
+             rest
+           })
+           
+           new_df<- reactive({
+             all_rest <- add_to_df()
+             top6<- head(all_rest,6)
+             top6
+           })
+           
+            dist_column<- reactive({
+              avail_rest<- new_df()
+              dist<- avail_rest$dist
+              dist
+            })
+            
+            if(all(dist_column()>0.05)){
+              output$recom<- renderText('Oops! There is no available restaurant around you!')}
+            else{leafletProxy('map',data = new_df())  %>% addMarkers(lng = ~LON, lat = ~LAT, icon=icon_rest,
+                          popup=~paste('Name:', NAME, '<br/>',
+                                       'Tel:', TEL, '<br/>',
+                                       'Zip:', ZIP, '<br/>',
+                                       'Add:', ADDRESS, '<br/>',
+                                       'Yelp.Star:', YELP.STAR, '<br/>'))
+                 output$recom<- renderText({
+                   recom_rest<-new_df() 
+                   paste('Name:', recom_rest[1,]$NAME,'\n',
+                         'Tel:', recom_rest[1,]$TEL,'\n',
+                         'Zip:', recom_rest[1,]$ZIP,'\n',
+                         'Add:', recom_rest[1,]$ADDRESS,'\n',
+                         'Yelp.Star:', recom_rest[1,]$YELP.STAR)
+                 })
+                }
+       
+         }
+         else{
+           
+           add_to_df<- reactive({
+             rest<- restaurant %>% dplyr::filter(TYPE==type) %>% 
+               mutate(dist = sqrt((LON-lng)^2 + (LAT-lat)^2)) %>% arrange(dist) 
+             rest
+           })
+           
+           new_df<- reactive({
+             all_rest<-add_to_df()
+             top15<-head(all_rest, 15) %>% arrange(desc(YELP.STAR))
+             top6<- head(top15, 6)
+             top6
+           })
+           
+           dist_column<- reactive({
+             avail_rest<-new_df()
+             dist<- avail_rest$dist
+             dist
+           })
+           
+           if(all(dist_column()>0.05)){
+             output$recom<- renderText('Oops! There is no available restaurant around you!')}
+           else{leafletProxy('map',data = new_df())  %>% addMarkers(lng = ~LON, lat = ~LAT, icon=icon_rest,
+                                                                  popup=~paste('Name:', NAME, '<br/>',
+                                                                               'Tel:', TEL, '<br/>',
+                                                                               'Zip:', ZIP, '<br/>',
+                                                                               'Add:', ADDRESS, '<br/>',
+                                                                               'Yelp.Star:', YELP.STAR, '<br/>'))
+                output$recom<- renderText({
+                  recom_rest<-new_df() 
+                  paste('Name:', recom_rest[1,]$NAME,'\n',
+                        'Tel:', recom_rest[1,]$TEL,'\n',
+                        'Zip:', recom_rest[1,]$ZIP,'\n',
+                        'Add:', recom_rest[1,]$ADDRESS,'\n',
+                        'Yelp.Star:', recom_rest[1,]$YELP.STAR)
+                })
+               }
+         }
+     })
+     
+     observeEvent(input$button4,{
+       leafletProxy('map') %>% clearMarkers()
+       hide('Recom')
+     })
+     
+     observeEvent(input$button5,{
+       leafletProxy('map') %>% clearMarkers()
+       hide('Recom')
+       hide('Resta')
+       hide('Sure')
+     })
 })
+
+
+
+
+
+
+
+
--- app/ui.r
+++ app/ui.r
@@ -1,42 +1,103 @@
+packages.used=c("leaflet","geosphere","shiny","shinydashboard","shinyjs","ggplot2")
+
+# check packages that need to be installed.
+packages.needed=setdiff(packages.used,
+                        intersect(installed.packages()[,1],
+                                  packages.used))
+# install additional packages
+if(length(packages.needed)>0){
+  install.packages(packages.needed, dependencies = TRUE)
+}
+
+
 library(shiny)
+library(shinydashboard)
 library(leaflet)
+library(ggplot2)
+library(shinyjs)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("2009 Manhattan Housing Sales"),
-  
-  # Sidebar with a selector input for neighborhood
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("nbhd", label = h5("Choose a Manhattan Neighborhood"), 
-                         choices = list("all neighborhoods"=0,
-                                        "Central Harlem"=1, 
-                                        "Chelsea and Clinton"=2,
-                                        "East Harlem"=3, 
-                                        "Gramercy Park and Murray Hill"=4,
-                                        "Greenwich Village and Soho"=5, 
-                                        "Lower Manhattan"=6,
-                                        "Lower East Side"=7, 
-                                        "Upper East Side"=8, 
-                                        "Upper West Side"=9,
-                                        "Inwood and Washington Heights"=10), 
-                         selected = 0)
-      #sliderInput("p.range", label=h3("Price Range (in thousands of dollars)"),
-      #            min = 0, max = 20000, value = c(200, 10000))
-    ),
-    # Show two panels
-    mainPanel(
-      #h4(textOutput("text")),
-      h3(code(textOutput("text1"))),
-      tabsetPanel(
-        # Panel 1 has three summary plots of sales. 
-        tabPanel("Sales summary", plotOutput("distPlot")), 
-        # Panel 2 has a map display of sales' distribution
-        tabPanel("Sales map", plotOutput("distPlot1"))),
-      leafletOutput("map", width = "80%", height = "400px")
-    )
- )
-))
 
+
+shinyUI(tagList(
+  useShinyjs(),
+  includeCSS("styles_new.css"),
+  navbarPage(
+    id='navbar',
+    inverse=TRUE, collapsible=TRUE, fluid=TRUE,
+    'Welcome, Art Foodie!',
+    tabPanel(title='Home', 
+             icon=icon('home'),
+             div(id='bg1'),
+             mainPanel(img(src='bg1.jpg'))),
+    tabPanel(title='About us',
+             icon=icon('user-circle-o'),
+             absolutePanel(div(id='title_content3','Hi there!This application was created by Tiantian Chen, Qian Shi, Yajie Guo and Stephanie Park. We are students from Columbia University who would like to help art-lovers find the best place to dine around major museums and theaters in New York City. Hope our App can help you find restaurants which match your preferences best. Enjoy your meal, Art Foodie!'))),
+    
+    tabPanel(title='App manual',
+             icon=icon('question-circle-o'),
+             absolutePanel(div(id='title_content4','How to use our App: Firstly, you need to choose destination of your travel---museum or theater? After selecting sure, you can choose what type of food you want, such as American, Italian, etc. Then we rank restaurants by grade or distance. After all these steps, a couple of restaurants which match your preferences will be shown. Finally, choose one and you are ready to go, enjoy!'),
+                           class='panel-default')),
+    
+    tabPanel(title='Let us try',
+             id='tab3',
+             icon=icon('hand-o-right'),
+             div(class='outer',
+                 leafletOutput("map", width="100%",height="100%"),
+                 
+                 absolutePanel(div(id='title_content','STEP 1: Choose An Art Destination'),
+                               class='panel-default',
+                               draggable=TRUE, fixed=TRUE,
+                               top=100, left=60, bottom='auto', right='auto',
+                               width=200, height=110,
+                               selectInput('destination', width=200,
+                                           div(id='label_content', 'What type do you like?'),
+                                           choices=c('Gallery','Museum','Theater','Library'), multiple=FALSE)),
+                 
+                 hidden(
+                   absolutePanel(id="Sure",
+                                 top=190, left=60, bottom='auto', right='auto',
+                                 width=100, height=80,
+                                 actionButton('button1','Confirm',width=100,
+                                              icon=icon('hand-o-right')),
+                                 actionButton('button2', 'Go back',width=100,
+                                              icon=icon('hand-o-left'))
+                   )),
+                 
+                 hidden(
+                   absolutePanel(div(id='title_content2','STEP 2: Choose Food Destination'),
+                                 class='panel-default',
+                                 id='Resta',draggable=TRUE, fixed=TRUE,
+                                 top=350, left=60, bottom='auto', right='auto',
+                                 width=200, height=190,
+                                 selectInput('type', div(id='label_content2','Categories:'),width=200,
+                                             choices=c('Dessert','American','QuickMeal',
+                                                       'Seafood','Italian','Asian','Mexician',
+                                                       'Chinese','European','French','Other'), multiple=FALSE),
+                                 selectInput('rank', div(id='label_content3','Ranking by:'),width=200,
+                                             choices=c('by Grade','by Distance'), multiple=FALSE),
+                                 actionButton('button3', 'Search ',width=100,
+                                              icon=icon('hand-o-right'))
+                   )
+                   
+                 ),
+                 hidden(
+                   absolutePanel(div(id='title_content3','STEP3: Your Recommendation'),
+                                 class='panel-default',
+                                 id='Recom', draggable=TRUE, fix=TRUE, 
+                                 top=60, right=60, left='auto', bottom='auto',
+                                 width=350, height=185,
+                                 verbatimTextOutput('recom'),
+                                 actionButton('button5', 'RESET ARTS', width=160,
+                                              icon=icon('hand-o-left')),
+                                 actionButton('button4', 'RESET RESTAURANTS', width=185,
+                                              icon=icon('hand-o-left'))
+                                 
+                   )
+                 )
+             ))
+    
+    
+    
+  ))
+  
+)
